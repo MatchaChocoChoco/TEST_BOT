f957cef748fe31c14db42cac1272733bdf80079d
ChocoChoco <58903140+MatchaChocoChoco@users.noreply.github.com>
2021-03-21 22:46:23 +0900

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -1,55 +1,78 @@
+import discord
 from discord.ext import commands
 import os
 import time
 
+
 TOKEN_PATH = 'TEST_BOT.token'
-CONSOLE_ID = 625271680184483853
+CONSOLE_ID = {'guild_id': 588391471493808139, 'chanel_id': 625271680184483853}
 
-bot = commands.Bot(command_prefix='/')
+cog_list = [
 
-# 接続時の処理
+]
+
+class Bot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.console_id = kwargs.get('console_id')
+    
+
+    # 接続時の処理
+    async def on_ready(self):
+        time_str = time.strftime("%Y/%m/%d %H:%M", time.strptime(time.ctime()))
+        print('---------------------------------------------------')
+        print('ログインしました', time_str)
+        print('---------------------------------------------------')
+        return
+
+    # メッセージ処理
+    async def on_message(self, message):
+        # 例外
+        if message.outher.bot: return
+
+        # メンション処理
+        if bot.user in message.mentions:
+            return await on_mention(message)
+
+        return
+
+    # メンション処理
+    async def on_mention(self, message):
+        await message.channel.send('現在調整中...', reference=message)
+        pass
+
+    # 切断時の処理
+    async def on_disconnect(self):
+        time_str = time.strftime("%Y/%m/%d %H:%M", time.strptime(time.ctime()))
+        print('---------------------------------------------------')
+        print('切断しました', time_str)
+        print('---------------------------------------------------')
+        return
 
 
-@bot.event
-async def on_ready():
-    time_str = time.strftime("%Y/%m/%d %H:%M", time.strptime(time.ctime()))
-    print('---------------------------------------------------')
-    print('ログインしました', time_str)
-    print('---------------------------------------------------')
-    return
-
-# メッセージ処理
-
-
-@bot.event
-async def on_message(message):
-    # 例外
-
-    if bot.user in message.mentions:
-        return await on_mention(message)
-
-
-async def on_mention(message):
-    await message.channel.send('現在調整中...')
-    pass
-
-
-# 切断時の処理
-@bot.event
-async def on_disconnect():
-    time_str = time.strftime("%Y/%m/%d %H:%M", time.strptime(time.ctime()))
-    print('---------------------------------------------------')
-    print('切断しました', time_str)
-    print('---------------------------------------------------')
-    return
-
+    def is_console_channel(self, message: discord.Message):
+        channel = message.channel
+        result = all([
+            channel.guild.id == CONSOLE_ID['guild_id'],
+            channel.id == CONSOLE_ID['channel_id']
+        ])
+        return result
 
 # token の取得
-if os.path.isfile(TOKEN_PATH):
-    with open(TOKEN_PATH) as f:
-        TOKEN = f.read()
-else:
-    print('トークンファイルが見つかりませんでした。')
-    exit()
+def get_token(TOKEN_PATH):
+    if os.path.isfile(TOKEN_PATH):
+        with open(TOKEN_PATH) as f:
+            TOKEN = f.read()
+        return TOKEN
+    else:
+        print('トークンファイルが見つかりませんでした。')
+        exit()
 
-bot.run(TOKEN)
+if __name__ == '__main__':
+    bot = Bot(command_prefix='/')
+    # cog のロード
+    for cog in cog_list:
+        bot.load_extension(cog)
+    
+    TOKEN = get_token(TOKEN_PATH)
+    bot.run(TOKEN)
